51c622f0dc27f0ec1a6cb9f6c5db57fe650b1916
FranN2442 <fgonzalezviejo@cifpfbmoll.eu>
2023-03-17 19:31:55 +0100

    .

--- examen2/src/main/java/edu/craptocraft/examen2/App.java
+++ examen2/src/main/java/edu/craptocraft/examen2/App.java
@@ -7,7 +7,7 @@ public class App
     public static void main( String[] args )
     {
         // Persona fran = new Persona("Fran", 18, "41622502T", 70.00, 1.74, 'H')
-        Persona gema = new Persona("Gema", 28, "1493478T", 70.00, 1.55, 'F');
+        Persona gema = new Persona("Gema", 28, "1493478T", 70.00, 1.70, 'F');
         System.out.println(gema.calcularIMC());
 
         System.out.println(gema.esMayorDeEdad());
